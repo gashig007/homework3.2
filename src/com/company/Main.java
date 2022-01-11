e41d8ccd9fd63a133327157c33872980aea9cede
gashig007 <rustamgashig@gmail.com>
2022-01-11 15:25:17 +0600

    set commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,19 +5,22 @@ public class Main {
     public static void main(String[] args) {
         Boolean remainder = false;
         BankAccount bankAccount = new BankAccount();
-        LimitException limitException = new LimitException("На вашем счету недостаточно денег ", bankAccount.getAmount());
+        LimitException limitException = null;
         bankAccount.deposit(20000);
         while (true) {
             try {
                 System.out.println("Ваш счет " + bankAccount.getAmount());
                 bankAccount.withDraw(6000);
+                if(bankAccount.getAmount() < 6000) {
+                    limitException = new LimitException("На вашем счету недостаточно денег ", bankAccount.withDraw(6000));
+                }
             } catch (LimitException limitException1) {
+                limitException1.printStackTrace();
                 if (limitException1.getRemainingAmount() < 6000) {
                     System.out.println("Ваш счет: " + limitException);
                     break;
                 }
             }
-
         }
     }
 }
