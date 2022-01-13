b02471002d229376250780d6e350ea154611bb20
gashig007 <rustamgashig@gmail.com>
2022-01-14 00:58:51 +0600

    set commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,11 @@ public class Main {
     public static void main(String[] args){
         BankAccount bankAccount = new BankAccount();
         bankAccount.deposit(20000);
-        Scanner scanner = new Scanner(System.in);
 
         while (true){
             try {
                 System.out.print("Напишите сумму вывода: ");
-                bankAccount.withDraw(scanner.nextInt());
+                bankAccount.withDraw(6000);
 
             }catch (LimitException limitException){
                 System.out.println(limitException.getMessage());
